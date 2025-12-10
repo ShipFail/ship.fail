c4b70b4cf5e2f711fcc7feb6040b10e1064a1d26
Huan Li <huan@chatie.io>
2025-12-10 01:57:10 +0000

    chore: remove deprecated test for non-existent folders

--- tests/deprecated-folder.test.ts
+++ tests/deprecated-folder.test.ts
@@ -8,11 +8,3 @@ Deno.test("jekyll content should live under docs/", () => {
   assert(existsSync(JEKYLL_FOLDERS.authors), "docs/_authors should exist");
   assert(existsSync(JEKYLL_FOLDERS.assets), "docs/assets should exist");
 });
-
-Deno.test("deprecated folders should not exist", () => {
-  const deprecated = ["jekyll", "_posts", "_contributors"];
-  for (const folder of deprecated) {
-    const full = `${repoRoot}/${folder}`;
-    assert(!existsSync(full), `${folder}/ should not exist`);
-  }
-});
